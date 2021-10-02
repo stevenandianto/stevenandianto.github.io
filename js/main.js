829590e2d9536fc1f3536bbecc0001781f237c59
Steven Andianto <stevenandianto@gmail.com>
2021-10-02 17:40:44 +0700

    hello pages

--- js/main.js
+++ js/main.js
@@ -5,3 +5,32 @@ window.addEventListener("scroll", function()
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.77 + "px";
 })
+
+// All hello languages
+const helloLanguages = [
+    "hello",
+    "halo",
+    "안녕하세요",
+    "你好",
+    "こんにちは",
+    "สวัสดี",
+    "hola",
+    "bonjour",
+    "Привет"
+];
+
+var counter = 0;
+var elem = document.getElementById("hello");
+setInterval(change, 3000);
+
+function change() {
+    elem.classList.add('fade-out');
+    setTimeout(function () {
+        elem.innerHTML = helloLanguages[counter];
+        elem.classList.remove('fade-out');
+        counter++;
+        if (counter >= helloLanguages.length) {
+            counter = 0;
+        }
+    }, 1000);
+}
